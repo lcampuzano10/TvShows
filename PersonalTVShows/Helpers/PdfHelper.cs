346d7d7cc43cc4b725ffb01398e73bc47a07d2a4
Luis C Casa <luis.campuzano32@hotmail.com>
2023-11-12 21:59:47 -0500

    adding pdf viewer

---
+++ PersonalTVShows/Helpers/PdfHelper.cs
@@ -0,0 +1,19 @@
+﻿using PersonalTVShows.Models.Documents;
+
+namespace PersonalTVShows.Helpers
+{
+    public static class PdfHelper
+    {
+        public static PdfList CreatePdfList()
+        {
+            PdfList PdfList = new()
+            {
+                new PdfDocument { Index = 1, PdfName = "Murach CSharp", PdfPath = @"" },
+                new PdfDocument { Index = 2, PdfName = "Murach Sql", PdfPath = @"" },
+                new PdfDocument { Index = 3, PdfName = "Blazor Web Development", PdfPath = @"" }
+            };
+
+            return PdfList;
+        }
+    }
+}
---
+++ PersonalTVShows/Models/Documents/PdfDocument.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalTVShows.Models.Documents
+{
+    public class PdfDocument
+    {
+        public int Index { get; set; }
+        public string PdfName { get; set; }
+        public string PdfPath { get; set; }
+    }
+}
